77510e71bb4a2156fc3fcb6dca4e2cb7d309dc9b
Burcu Kacak <burcu.kacakk@gmail.com>
2023-07-24 22:25:37 +0300

    BurcuBV1

--- src/test/java/tests/US001/TC01_AramaKutusuTesti.java
+++ src/test/java/tests/US001/TC01_AramaKutusuTesti.java
@@ -13,6 +13,7 @@ public class TC01_AramaKutusuTesti {
         Page page = new Page();
         page.aramaKutusu.sendKeys("iphone", Keys.ENTER);
         Driver.closeDriver();
+        //Amazon sayfasina gidilip arama kutusu test edildi
 
     }
 }
@@ -26,4 +27,23 @@ yaz
 //git log versionlari gormek icin
 //git log --oneline versiyonlari ayni satirda gormek icin
 //bir kez add yaptiysak git commit-am"version ismi" ile direk commitleyebiliriz
+//versiyonladiktan sonra bekletmenin bir anlami yok isimiz bitince push edelim
+
+/*dosyalari alirken  url copy edince masaustunde bir klasor ac, cmd'den
+git init
+url yapistir
+git pull  yapinca src altindaki dosyalari almis oluruz
+
+clon yaparken url copy edince masaustunde bir klasor ac, cmd'den
+git clone url yapistir enterla o zaman proje komple gelir
+ */
+
+/*
+Branch olusturma
+git branch isim
+git log --oneline dersek branch hangisindeyse o onde olur
+git checkout isim dersek ismi verilen branch e gecer
+
+ */
+
 
