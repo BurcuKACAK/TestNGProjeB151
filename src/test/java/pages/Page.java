c28a6188889c4f775325b1dab863239837721c01
Burcu Kacak <burcu.kacakk@gmail.com>
2023-07-24 22:00:13 +0300

    US001_V1

---
+++ src/test/java/pages/Page.java
@@ -0,0 +1,115 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class Page {
+    public Page() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    //Burcu Hanim
+    @FindBy(id = "twotabsearchtextbox")
+    public WebElement aramaKutusu;
+
+    //Herkesin locate aldigi alan ayri olmali. Conflict olmamasi icin. Ama isterse bir kisi digerinin locate'ini kullanabilir
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Burcu Hanim
+    //Ayca Hanim
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Ayca Hanim
+
+    //Burcu Hanim
+
+
+
+
+
+
+
+
+
+
+    //Burcu Hanim
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,79 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+    /*
+    POM(Page Object Model)
+        Test senaryolarinin daha az kod ile yazilmasina ve bakiminin daha kolay yapilmasina
+        olanak saglayan yazilim test yontemidir. TestNG de ve CUCUMBER frameworklerinde POM kalibi kullanilir.
+     */
+
+public class Driver {
+    private Driver(){
+        /*
+       Driver class'indan obje olusturmanin onunce gecebilmek icin
+       default constructor'i private yaparak bunu engellemis oluruz. Bu kaliba da Singleton pattern denir
+       */
+        // default olan constructor'i private yaparak baska package altinda  bu class'tan obje olusturmasını engelliyoruz. Singleton pattern
+//--> new'lenemez.
+//--> bunun icin kendi class'ina default constructor yazdik.
+//--> bu default constructor'in access modifier'i
+//--> private yapildigi icin baska class'larda Driver class'indan obje olusturulamaz.
+    }
+    static WebDriver driver;
+
+    public static WebDriver getDriver(){
+         /*
+            Driver 'i her cagirdigimizda yeni bir pencere acilmasinin onune gecmek icin
+         if blogu icinde Eger driver'a deger ATANMAMISSA deger ata, eger deger atanmamissa
+         Driver'i ayni sayfada return et.
+           */
+        /*
+        Properties dosyasinda key olarak belirttigimiz browser'a asagidaki gibi hangi degeri
+        belirtirsek testlerimi o browser ile calisir
+         */
+
+      if(driver == null) {//--> Driver'a deger atanmamissa
+          switch (ConfigReader.getProperty("browser")){
+              case "chrome" :
+                  WebDriverManager.chromedriver().setup();
+                  driver = new ChromeDriver();
+                  break;
+              case "edge":
+                  WebDriverManager.edgedriver().setup();
+                  driver= new EdgeDriver();
+                  break;
+              case  "safari" :
+                  WebDriverManager.safaridriver().setup();
+                  driver = new SafariDriver();
+                  break;
+              default:
+                  WebDriverManager.chromedriver().setup();
+                  driver = new ChromeDriver();
+          }
+          driver.manage().window().maximize();
+          driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+      }
+       return driver;
+
+    }
+    public static void closeDriver(){
+       if(driver !=null) {//-->Driver'a deger atanmissa, bos degilse
+           driver.close();
+           driver = null;//-->Driver'i kapattiktan sonra bosalt
+       }
+    }
+    public static void quitDriver(){
+        if(driver !=null) {//-->Driver'a deger atanmissa, bos degilse
+            driver.quit();
+            driver = null;//-->Driver'i kapattiktan sonra bosalt
+        }
+    }
+
+
+}
